a4b2489159e0cb6f38467122ce3afd25617b60ab
Roj <roj@rojser.best>
2021-12-07 19:49:30 +0300

    Update loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,3 +1,28 @@
 export default function Loader() {
-  return <p>Loading...</p>;
+  return (
+    <svg
+      className="block w-10 h-10 mx-auto"
+      viewBox="0 0 100 100"
+      preserveAspectRatio="xMidYMid"
+    >
+      <circle
+        cx="50"
+        cy="50"
+        fill="none"
+        stroke="#222222"
+        stroke-width="10"
+        r="35"
+        stroke-dasharray="164.93361431346415 56.97787143782138"
+      >
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          repeatCount="indefinite"
+          dur="1s"
+          values="0 50 50;360 50 50"
+          keyTimes="0;1"
+        ></animateTransform>
+      </circle>
+    </svg>
+  );
 }
